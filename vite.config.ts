f05c5e0a607456886f1fab5386ef5d5c94895eb1
大岩祐介 <YUSUKE@MacBook-Air.local>
2025-10-08 11:21:55 +0900

    fix: adjust base path dynamically

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/marumie/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    sourcemap: true
-  }
-})
+export default defineConfig(() => {
+  const basePath = process.env.GITHUB_ACTIONS ? '/marumie/' : '/';
+
+  return {
+    plugins: [react()],
+    base: basePath,
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+      sourcemap: true
+    }
+  };
+});
